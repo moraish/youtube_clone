b5fa893023a73c74d0206779c8d443e032e19a9e
Moraish Kapoor <moraish.kapoor@hotmail.com>
2025-01-22 15:49:26 -0500

    updates to the video card - added avatar

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,20 +1,49 @@
-import { Paper } from "@mui/material";
+import { Avatar, Card, CardContent, Paper, Typography, Box } from "@mui/material";
 // import Grid from '@mui/material/Grid2';
-import { Grid2 } from "@mui/material";
+import { Grid2, CardMedia } from "@mui/material";
 
 function VideoCard({ thumbnail, channel_icon, title, channel_name, views, launch_time }) {
     return (
         <>
             <Grid2 item="true" size={{ xs: 12, sm: 6, md: 4, lg: 3, xl: 2 }}>
-                <Paper elevation={0}>
-                    <img
-                        src={thumbnail}
+                {/* Thumbnail Section */}
+                <Card elevation={0}>
+                    <CardMedia
+                        component="img"
+                        image={thumbnail}
                         alt="video thumbnail"
+                        sx={{
+                            width: '100%',
+                            aspectRatio: '16/9',
+                            borderRadius: 2
+                        }}
+
                     />
-                    {title}
-                    {channel_name}
-                    {views}
-                </Paper>
+
+                    {/* Content Section */}
+                    <CardContent sx={{
+                        p: 0,
+                        display: 'flex',
+                        gap: 1.5
+                    }}>
+                        <Avatar sx={{
+                            width: 36, height: 36, mt: 0.5
+                        }}>{channel_name[0]}</Avatar>
+                        <Box sx={{ flex: 1 }}>
+                            <Typography variant="subtitle2"
+                                sx={{
+                                    fontWeight: 500
+                                }}>{title}</Typography>
+                            <Typography variant="body2" sx={{
+                                fontSize: '0.9rem'
+                            }}>{channel_name}</Typography>
+                            <Typography variant="body2" sx={{
+                                fontSize: '0.9rem'
+                            }}>{views}</Typography>
+                        </Box>
+                    </CardContent>
+
+                </Card>
             </Grid2>
         </>
     )
